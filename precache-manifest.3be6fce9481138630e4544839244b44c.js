ce78ee7eb509b62b9402efacbe9a8339b2021000
Tolokoban <contact@tolokoban.org>
2020-07-12 19:43:30 +0200

    Updates

---
+++ precache-manifest.3be6fce9481138630e4544839244b44c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c737d802bdc48aff04b44aad259578c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de5a3da9c31a17e567f4",
+    "url": "./static/css/2.a6193429.chunk.css"
+  },
+  {
+    "revision": "97f763fcd5966a5c2172",
+    "url": "./static/css/3.663ed3ea.chunk.css"
+  },
+  {
+    "revision": "ae424c99bd00b1acf632",
+    "url": "./static/css/4.1b5b2257.chunk.css"
+  },
+  {
+    "revision": "ee92517072676315e4f7",
+    "url": "./static/css/5.1b5b2257.chunk.css"
+  },
+  {
+    "revision": "1942b8472fec7aa40c0c",
+    "url": "./static/css/6.9b9ae968.chunk.css"
+  },
+  {
+    "revision": "1f318121238cf5c56890",
+    "url": "./static/css/main.2bc9af5e.chunk.css"
+  },
+  {
+    "revision": "de5a3da9c31a17e567f4",
+    "url": "./static/js/2.5396a2f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5396a2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f763fcd5966a5c2172",
+    "url": "./static/js/3.990f6221.chunk.js"
+  },
+  {
+    "revision": "ae424c99bd00b1acf632",
+    "url": "./static/js/4.c4d09114.chunk.js"
+  },
+  {
+    "revision": "ee92517072676315e4f7",
+    "url": "./static/js/5.5a5eedf8.chunk.js"
+  },
+  {
+    "revision": "1942b8472fec7aa40c0c",
+    "url": "./static/js/6.cee3b0f4.chunk.js"
+  },
+  {
+    "revision": "1f318121238cf5c56890",
+    "url": "./static/js/main.5bbd480c.chunk.js"
+  },
+  {
+    "revision": "cac5a3d7b6138a64490f",
+    "url": "./static/js/runtime-main.923d1ba1.js"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "./static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "./static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "./static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "99be908bf9c2d4217ea763e64f5694d6",
+    "url": "./static/media/dialog.99be908b.yaml"
+  },
+  {
+    "revision": "ed7190eadf3846a05e7238597dfe0c3b",
+    "url": "./static/media/input-image.ed7190ea.yaml"
+  },
+  {
+    "revision": "7270440b8ddb42ebfe9fba9297a3563d",
+    "url": "./static/media/josefin.7270440b.woff2"
+  },
+  {
+    "revision": "04c7e99b2918bcab6bebc7a5c44459c5",
+    "url": "./static/media/mystery-quest-400.04c7e99b.woff2"
+  },
+  {
+    "revision": "d73f403cb7dedbcc76e42e5d81cb4e9b",
+    "url": "./static/media/wheel.d73f403c.png"
+  }
+]);
